c738b308413b1cb0e309331f5539cd69dbe74ff8
Pavel Veretennikov <vermut@kid.lv>
2016-04-14 02:43:01 +0300

    rewrite for non-wifi

--- HCSR04_Interrupts.ino
+++ HCSR04_Interrupts.ino
@@ -8,13 +8,13 @@ void net_report(int lastVal);
 #define GND          D1
 
 ADC_MODE(ADC_VCC);
-int startUpDistance = -1;
-long nextReport;
+float startUpDistance = -1;
 
 void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
-  Serial.setDebugOutput(true);
-  net_setup();
+  // Serial.setDebugOutput(true);
+  net_enable();
+  net_wait_connect();
 
   // Power setup
   pinMode(VCC, OUTPUT);
@@ -25,29 +25,92 @@ void setup() {
   Plugin_013_Init();
   startUpDistance = Plugin_013_Median(20);
 
-  Serial.print("Startup: ");
+  /* Serial.print("Startup: ");
   Serial.print(startUpDistance);
-  Serial.println("cm");
+  Serial.println("cm"); */
 
-  net_register(startUpDistance);
-  net_report(startUpDistance);
-  resetNextReport();
+  // net_register(startUpDistance);
+  // net_report(startUpDistance);
+  // resetNextReport();
+
+  net_disable();
 }
 
 void loop() {
-  delay(15);
-  int curVal = Plugin_013_read();
+  pingSonar();
+  reportIfNeccessary();
+  handleWifi();  
+}
+
+int alertPings = 0;
+void pingSonar() {
+  float curVal =Plugin_013_read();
   if (nonStartUpDistance(curVal)) {
-    int nextVal[10];
+    alertPings++;
+  }
+}
+
+boolean timeForReport = false;
+long nextReport = 0;
+void reportIfNeccessary() {
+  if (alertPings > 0 || millis() > nextReport) {
+    timeForReport = true;
+    wifiState = WifiState.DO_CONNECT;
+  }   
+}
+
+enum WifiState {
+  OFF,
+  DO_CONNECT,
+  CONNECTING,
+  DO_REPORT
+};
+WifiState wifiState = WifiState.OFF;
+void handleWifi() {
+  switch (wifiState):
+    case WifiState.OFF: return;
+
+    case WifiState.DO_CONNECT:
+      net_enable();
+      wifiState = WifiState.CONNECTING;
+      break;
+
+    case WifiState.CONNECTING:
+      if (net_is_connected())
+         wifiState = WifiState.DO_REPORT;
+      break;
+      
+    case WifiState.DO_REPORT:
+      net_report(alertPings);
+      net_disable();
+      reportSucceeded();
+      break;
+  }    
+}
+
+void reportSucceeded() {
+  nextReport = millis() + 60 * 1000;
+  wifiState = WifiState.OFF;
+  timeForReport = false;
+  alertPings = 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void loopy() {
+  // delay(15);
+  float curVal =Plugin_013_read();
+  if (curVal < 0 || curVal > 220)
+    curVal = 220;
+    
+  if (false && nonStartUpDistance(curVal)) {
+    float nextVal[10];
     nextVal[0] = curVal;
     int i = 1;
     for (; i < 10; i++)    {
       delay(15);
       nextVal[i] = Plugin_013_read();
-      if (/* !correlates(startUpDistance, nextVal[i]) && */ correlates(nextVal[i], nextVal[i - 1]))      {
-        /* curVal = nextVal; */
-      }
-      else
+      if (/* !correlates(startUpDistance, nextVal[i]) && */ !correlates(nextVal[i], nextVal[i - 1]))
         break;
     }
 
@@ -61,8 +124,8 @@ void loop() {
     }
 
   } else {
-    // Serial.print(curVal);
-    // Serial.println(",0");
+    Serial.print(curVal);
+    Serial.println(",0");
   }
 
   /* if (reportable_reading(curVal)/* && multiCheck(10)* /) {
@@ -83,25 +146,12 @@ void loop() {
     } */
 }
 
-boolean correlates(int base, int value) {
-  return (abs(base - value) <= 2);
+boolean correlates(float base, float value) {
+  return (abs(base - value) <= 5);
 }
 
 boolean nonStartUpDistance(float reading) {
   return (reading > 0 && abs(reading - startUpDistance) > 5);
-}
-
-#define THRESHOLD 0.3
-boolean multiCheck(int amount_to_check) {
-  float reportable = 0;
-  for (int i = 0; i < amount_to_check; i++) {
-    if (reportable_reading(Plugin_013_read()))
-      reportable++;
-  }
-
-  Serial.print("Percentage: ");
-  Serial.println(reportable / amount_to_check);
-  return ((reportable / amount_to_check) > THRESHOLD);
 }
 
 void resetNextReport() {
